2184725d861afc9885abac9b0035de6d264212af
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-07-30 16:21:53 +0700

    feat: category entity

---
+++ lib/shared/categories/domain/entities/category.dart
@@ -0,0 +1,8 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'category.freezed.dart';
+
+@freezed
+class Category with _$Category {
+  const factory Category() = _Category;
+}
