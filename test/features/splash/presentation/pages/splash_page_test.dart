1ec9c5ce03d0dc6bdeb050715560b70a5d6a85b8
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-07-30 12:41:11 +0700

    test: fixing splash page test

--- test/features/splash/presentation/pages/splash_page_test.dart
+++ test/features/splash/presentation/pages/splash_page_test.dart
@@ -1,12 +1,7 @@
 import 'package:expense_tracker/features/splash/presentation/pages/splash_page.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../../../helpers/helpers.dart';
-
 void main() {
-  setUpAll(() async {
-    await configureInjector();
-  });
   group('Splash Page', () {
     testWidgets(
       'Displayed',
