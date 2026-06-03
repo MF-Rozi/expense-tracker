0c053edc6fc73ee9319404e79065c06e33c9185b
MF-Rozi <m.fakhrulrozi10@gmail.com>
2026-06-04 00:37:04 +0700

    feat: implement category repository and use cases for managing categories

---
+++ lib/features/category/domain/entities/category.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:template/shared/domain/entities/value_objects.dart';
+
+class Category extends Equatable {
+  const Category({
+    required this.uuid,
+    required this.name,
+    required this.isSynced,
+    required this.updatedAt,
+    this.parentUuid,
+  });
+
+  final UniqueId uuid;
+  final StringSingleLine name;
+  final UniqueId? parentUuid;
+  final bool isSynced;
+  final DateTime updatedAt;
+
+  bool get isRoot => parentUuid == null;
+
+  @override
+  List<Object?> get props => [
+        uuid,
+        name,
+        parentUuid,
+        isSynced,
+        updatedAt,
+      ];
+}
